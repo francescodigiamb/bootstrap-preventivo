6e72b78a1fe10df530f7cf391476f6020f946524
francescodgiamb <francescodigiamb@gmail.com>
2024-07-31 22:59:37 +0200

    ADD:js-bloccato-invio-form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 console.log ('Milestone-1')
+
+const formElement = document.getElementById ('form')
+console.log (formElement)
+
+formElement.addEventListener('submit', function (event) {
+    // blocco l'invio del form
+    event.preventDefault()
+    console.log('submit del form')
+})
